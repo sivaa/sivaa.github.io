deea415786703101cc9c3787f0ab3d879f3481d6
Sivasubramaniam Arunachalam <siva@sivaa.in>
2018-05-04 16:09:19 +0200

    Base + About Me Draft

---
+++ content/about-me.md
@@ -0,0 +1,11 @@
+Title: About me
+Date: 2018-05-04 15:00
+Modified: 2018-05-04 15:00
+Category: misc
+Tags: about, misc
+Slug: about-me
+Authors: Sivasubramaniam Arunachalam
+Summary: About me
+
+I'm **Sivasubramaniam(Siva) Arunachalam** (Tamil: சிவசுப்ரமணியம் அருணாசலம்), a software engineer based on Berlin, Germany.
+
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Sivasubramaniam Arunachalam'
+SITENAME = 'Sivasubramaniam Arunachalam'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+STATIC_PATHS = ['images']

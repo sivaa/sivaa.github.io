a90f3e06dbed3426aeed3cb9eccffc355942f133
Sivasubramaniam Arunachalam <siva@sivaa.in>
2018-05-05 23:26:15 +0200

    favicon

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,9 +39,34 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-STATIC_PATHS = ['images', 'extra/CNAME']
+
+STATIC_PATHS = [
+    'images',
+    'extra/CNAME',
+
+    'images/favicon/favicon.ico',
+    'images/favicon/android-chrome-144x144.png',
+    'images/favicon/apple-touch-icon.png',
+    'images/favicon/browserconfig.xml',
+    'images/favicon/favicon-16x16.png',
+    'images/favicon/favicon-32x32.png',
+    'images/favicon/mstile-150x150.png',
+    'images/favicon/safari-pinned-tab.svg',
+    'images/favicon/site.webmanifest',
+]
+
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
+
+    'images/favicon/favicon.ico': {'path': 'favicon.ico'},
+    'images/favicon/android-chrome-144x144.png': {'path': 'android-chrome-144x144.png'},
+    'images/favicon/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'images/favicon/browserconfig.xml': {'path': 'browserconfig.xml'},
+    'images/favicon/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+    'images/favicon/favicon-32x32.png': {'path': 'favicon-32x32.png'},
+    'images/favicon/mstile-150x150.png': {'path': 'mstile-150x150.png'},
+    'images/favicon/safari-pinned-tab.svg': {'path': 'safari-pinned-tab.svg'},
+    'images/favicon/site.webmanifest': {'path': 'site.webmanifest'},
 }
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
--- publishconf.py
+++ publishconf.py
@@ -11,6 +11,7 @@ import os
 sys.path.append(os.curdir)
 
 SITEURL = 'https://sivaa.in'
+SITENAME = 'Sivasubramaniam Arunachalam'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -25,9 +26,33 @@ DELETE_OUTPUT_DIRECTORY = True
 
 TIMEZONE = 'Europe/Berlin'
 
-STATIC_PATHS = ['images', 'extra/CNAME']
+STATIC_PATHS = [
+    'images',
+    'extra/CNAME',
+
+    'images/favicon/favicon.ico',
+    'images/favicon/android-chrome-144x144.png',
+    'images/favicon/apple-touch-icon.png',
+    'images/favicon/browserconfig.xml',
+    'images/favicon/favicon-16x16.png',
+    'images/favicon/favicon-32x32.png',
+    'images/favicon/mstile-150x150.png',
+    'images/favicon/safari-pinned-tab.svg',
+    'images/favicon/site.webmanifest',
+]
+
 EXTRA_PATH_METADATA = {
     'extra/CNAME': {'path': 'CNAME'},
+
+    'images/favicon/favicon.ico': {'path': 'favicon.ico'},
+    'images/favicon/android-chrome-144x144.png': {'path': 'android-chrome-144x144.png'},
+    'images/favicon/apple-touch-icon.png': {'path': 'apple-touch-icon.png'},
+    'images/favicon/browserconfig.xml': {'path': 'browserconfig.xml'},
+    'images/favicon/favicon-16x16.png': {'path': 'favicon-16x16.png'},
+    'images/favicon/favicon-32x32.png': {'path': 'favicon-32x32.png'},
+    'images/favicon/mstile-150x150.png': {'path': 'mstile-150x150.png'},
+    'images/favicon/safari-pinned-tab.svg': {'path': 'safari-pinned-tab.svg'},
+    'images/favicon/site.webmanifest': {'path': 'site.webmanifest'},
 }
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
